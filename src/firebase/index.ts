b6287cd88ef172e65f0efccc96fe5768d8bef598
vishwa-akshat <akvs0707@gmail.com>
2023-05-16 21:01:24 +0530

    Added Firebase Setup and SASS

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getStorage, ref } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOeGWI64p9Bln_3Z759F90W7YKCXhKjjk",
+    authDomain: "image-uploader-e0f67.firebaseapp.com",
+    projectId: "image-uploader-e0f67",
+    storageBucket: "image-uploader-e0f67.appspot.com",
+    messagingSenderId: "776831422376",
+    appId: "1:776831422376:web:3139e9631c05073885d79d",
+};
+
+export const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage();
+
+export const storageRef = (name: { name: string }) => ref(storage, name);
